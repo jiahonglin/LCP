c48019f0bfc70bcb548808a58d21c1b0f6b735d4
Jiahong Lin <jiahonglin17@gmail.com>
2019-06-22 18:23:31 +0800

    LeedCode Practice question 704.
    update for method 2.

--- LCP/lcp_704.cpp
+++ LCP/lcp_704.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++)
+      
+        return searchTarget(nums, target, 0, nums.size()-1);
+    }
+    int searchTarget(vector<int>& num, int target, int start, int end)
+    {
+        if(start > end) return -1;
+        int mid = start + (end - start)/2;
+        if(num[mid] == target) return mid;
+        else if(num[mid] > target)
         {
-            if(nums[i] == target)
-            {
-                return i;
-            }
+            return searchTarget(num,target,start,mid-1);
         }
-        return -1;
+        else
+        {
+            return searchTarget(num,target,mid+1,end);
+        }
     }
 };
