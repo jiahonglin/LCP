9f6c1008c18715c6725e589e24d3fe3128fc22f5
Jiahong Lin <jiahonglin17@gmail.com>
2019-06-26 01:51:32 +0800

    LeedCode Practice question 033.

---
+++ LCP/lcp_033.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1,mid=0;
+        while(right >= left)
+        {
+            mid = left + (right-left)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] > nums[right])
+            {
+                if(nums[mid] > target && nums[left] <= target) 
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+            else
+            {
+                if(nums[mid] < target && nums[right]>=target)
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+        }
+        return -1;
+    }
+};
