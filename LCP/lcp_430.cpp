b4110e1e6fd17a6fba6f88ca178d4d37e4037850
Jiahong Lin <jiahonglin17@gmail.com>
2021-04-08 23:42:16 +0800

    LeedCode Practice question 430.

---
+++ LCP/lcp_430.cpp
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        Node *cur = head;
+        while(cur){
+            Node *next = cur->next;
+            if(cur->child){
+                cur->child = flatten(cur->child);
+                cur->next = cur->child;
+                cur->next->prev = cur;
+                Node *last = cur->child;
+                cur->child = NULL;
+                while(last->next) last = last->next;
+                last->next = next;
+                if(next) next->prev = last;
+            }
+            cur = cur->next;
+        }
+        return head;
+    }
+};
