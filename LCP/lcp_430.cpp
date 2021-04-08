ad61f835997e2bfe927436207e641081f7ba5885
Jiahong Lin <jiahonglin17@gmail.com>
2021-04-08 23:51:26 +0800

    LeedCode Practice question 430.

--- LCP/lcp_430.cpp
+++ LCP/lcp_430.cpp
@@ -14,6 +14,23 @@ public:
     Node* flatten(Node* head) {
         Node *cur = head;
         while(cur){
+            if(cur->child){
+                Node *next = cur->next;
+                Node *last = cur->child;
+                while(last->next) last = last->next;
+                cur->next = cur->child;
+                cur->next->prev = cur;
+                cur->child = NULL;
+                last->next = next;
+                if(next) next->prev = last;
+            }
+            cur = cur->next;
+        }
+        return head;
+
+        /*
+        Node *cur = head;
+        while(cur){
             Node *next = cur->next;
             if(cur->child){
                 cur->child = flatten(cur->child);
@@ -28,5 +45,6 @@ public:
             cur = cur->next;
         }
         return head;
+        */
     }
 };
