699fb20120b5f1a684ed0bd1c07104e165d73f19
Jiahong Lin <jiahonglin17@gmail.com>
2021-07-23 00:30:50 +0800

    LeedCode Practice question 989.

---
+++ LCP/lcp_989.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int> res;
+        for(int i=num.size()-1;i>=0;i--){
+            res.insert(res.begin(),(num[i]+k)%10);
+            k=(num[i]+k)/10;
+        }
+        while(k>0){
+            res.insert(res.begin(),k%10);
+            k/=10;
+        }
+        return res;
+    }
+};
