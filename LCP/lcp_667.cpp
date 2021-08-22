90f3eccf4325e20df86b36d079dab6a72fefecd9
Jiahong Lin <jiahonglin17@gmail.com>
2021-08-22 18:35:03 +0800

    LeedCode Practice question 667.
    LeedCode Practice question 1162.# Please enter the commit message for your changes. Lines starting

---
+++ LCP/lcp_667.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> constructArray(int n, int k) {
+
+        vector<int> res;
+        int i=1;
+        int j=n;
+        while(i<=j){
+            if(k>1) res.push_back(k--%2?i++:j--);
+            else res.push_back(i++);
+        }
+        return res;
+    }
+};
