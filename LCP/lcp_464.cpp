e395553ecdbedd29ba0556bba89370604027f30c
Jiahong Lin <jiahonglin17@gmail.com>
2021-10-03 00:31:31 +0800

    LeedCode Practice question 464.

--- LCP/lcp_464.cpp
+++ LCP/lcp_464.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    vector<char> v; //status:0(unknown), 1(win), -1(lose)
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        if(desiredTotal <= maxChoosableInteger) return true;
+        if(desiredTotal > (maxChoosableInteger*(maxChoosableInteger+1)/2)) return false;
+        v=vector<char>(1<<maxChoosableInteger,0);
+        return canIwin(maxChoosableInteger, desiredTotal, 0);
+    }
+    bool canIwin(int M, int T, int state){
+        
+        if(T<=0) return false;
+        if(v[state]!=0) return v[state]==1;
+        for(int i=0;i<M;i++){
+            if(state & (1<<i)) continue;
+            if(!canIwin(M,T-(i+1), (state|(1<<i)))){
+                v[state] = 1;
+                return true;
+            }
+        }
+        v[state]=-1;
+        return false;
+    }
+    /*
     bool canIWin(int maxChoosableInteger, int desiredTotal) {
         if(desiredTotal <= maxChoosableInteger) return true;
         if(desiredTotal > (maxChoosableInteger*(maxChoosableInteger+1)/2)) return false;
@@ -8,10 +30,10 @@ public:
     }
     bool canIwin(int maxInt, int desire, int use, unordered_map<int,bool>& mp){
         if(mp.count(use)) return mp[use];
-        for(int i=0;i<maxInt;i++){
+        for(int i=1;i<=maxInt;i++){
             int cur=(1<<i);
             if((cur & use) == 0){
-                if(desire <= i+1 || !canIwin(maxInt,desire-(i+1),cur|use, mp)){
+                if(desire <= i || !canIwin(maxInt,desire-i,cur|use, mp)){
                     mp[use]=true;
                     return true;
                 }
@@ -20,4 +42,5 @@ public:
         mp[use]=false;
         return false;
     }
+    */
 };
