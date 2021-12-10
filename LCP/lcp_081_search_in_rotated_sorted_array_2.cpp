61202101d9326869d385e6ff26d852b7e65efd0f
jiahong <jiahonglin17@gmail.com>
2021-12-10 20:52:04 +0800

    LeedCode Practice question lcp_081_search_in_rotated_sorted_array_2.cpp

---
+++ LCP/lcp_081_search_in_rotated_sorted_array_2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1;
+        int mid=0;
+        while(left <= right){
+            mid=left+(right-left)/2;
+            if(nums[mid]==target) return true;
+            if(nums[left] < nums[mid]){
+                if(target < nums[mid] && target >= nums[left]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+            else if(nums[left] > nums[mid]){
+                if(target > nums[mid] && target <= nums[right]){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+            }
+            else{
+                left++;
+            }
+        }
+        return false;
+    }
+};
