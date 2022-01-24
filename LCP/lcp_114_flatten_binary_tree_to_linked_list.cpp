bab85d3a9f0ffd539f0985cd252264e9d49c0a5d
jiahong <jiahonglin17@gmail.com>
2022-01-25 01:15:38 +0800

    LeedCode Practice question lcp_114_flatten_binary_tree_to_linked_list

---
+++ LCP/lcp_114_flatten_binary_tree_to_linked_list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root) return;
+        flatten(root->left);
+        flatten(root->right);
+        TreeNode* tmp=root->right;
+        root->right=root->left;
+        root->left=NULL;
+        while(root->right) root=root->right;
+        root->right=tmp;
+    }
+    /*
+    TreeNode* prev;
+    void flatten(TreeNode* root) {
+        if(!root) return;
+        flatten(root->right);
+        flatten(root->left);
+        root->right=prev;
+        root->left=NULL;
+        prev=root;
+    }
+    */
+
+};
