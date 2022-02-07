4f9b26aeafb015f88a6a0e3d27bc4f54e6e1662b
jiahong <jiahonglin17@gmail.com>
2022-02-07 21:07:45 +0800

    LeedCode Practice question lcp_189_rotate_array

---
+++ LCP/lcp_189_rotate_array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(k==0) return;
+        while(k>nums.size()) k=k%nums.size();
+        vector<int> v(nums.size(),0);
+        int a=0,b=nums.size()-k;
+        for(int i=0;i<nums.size();i++){
+            if(b<=nums.size()-1){
+                v[i]=nums[b];
+                b++;
+            }
+            else{
+                v[i]=nums[a];
+                a++;
+            }
+        }
+        nums=v;
+    }
+};
