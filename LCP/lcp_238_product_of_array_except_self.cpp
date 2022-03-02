82274be639fe51b58a95a6f798ae3d31b7837b72
jiahong <jiahonglin17@gmail.com>
2022-03-02 23:46:37 +0800

    LeedCode Practice question lcp_238_product_of_array_except_self

---
+++ LCP/lcp_238_product_of_array_except_self.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> frontProduct(n,1);
+        vector<int> backProduct(n,1);
+        for(int i=0;i<n-1;i++){
+            frontProduct[i+1] = frontProduct[i] * nums[i];
+        }
+        for(int i=n-1;i>0;i--){
+            backProduct[i-1] = backProduct[i] * nums[i];
+        }
+        
+        for(int i=0;i<n;i++){
+            nums[i] = frontProduct[i]*backProduct[i];
+        }
+        return nums;
+    }
+};
