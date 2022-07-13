a478e48700401d9811b3d9ebfc0139b756ab21c9
jiahong <jiahonglin17@gmail.com>
2022-07-14 00:19:54 +0800

    LeedCode Practice question lcp_018_4sum

--- LCP/lcp_018_4sum.cpp
+++ LCP/lcp_018_4sum.cpp
@@ -1,6 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n  = nums.size();
+        vector<vector<int>> sol;
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<n-3;i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1;j<n-2;j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+                int left = j+1;
+                int right = n-1;
+                while(left < right){
+                    long sum = (long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum == target){
+                        vector<int> v{nums[i],nums[j],nums[left],nums[right]};
+                        sol.push_back(v);
+                        while(left < right && nums[left] == nums[left+1]) left++;
+                        while(left < right && nums[right] == nums[right-1]) right--;
+                        left++;
+                        right--;
+                    }
+                    else if(sum < target){
+                        left++;
+                    }
+                    else{
+                        right--;
+                    }
+                }
+            }
+        }
+        return sol;
+    }
+};
+
+
+/*class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> sol;
         if(nums.size()<4) return {};
         sort(nums.begin(),nums.end());
@@ -32,4 +68,4 @@ public:
         }
         return ret;
     }
-};
+};*/
