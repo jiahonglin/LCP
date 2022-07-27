93ed4a92e768dc7e1097bae9ec06a97042a5a53b
jiahong <jiahonglin17@gmail.com>
2022-07-28 00:40:51 +0800

    LeedCode Practice question lcp_031_next_permutation

--- LCP/lcp_031_next_permutation.cpp
+++ LCP/lcp_031_next_permutation.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n-2;
+        int j = n-1;
+        while(i>=0 && nums[i+1] <= nums[i]) i--;
+        if(i >= 0){
+            while(j > i && nums[j] <= nums[i]) j--;
+            swap(nums[i],nums[j]);
+        }
+        reverse(nums.begin()+i+1,nums.end());
+    }
+    /*
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         for(int i = n-2;i>=0;i--){
@@ -15,4 +28,5 @@ public:
         }
         reverse(nums.begin(),nums.end());
     }
+    */
 };
