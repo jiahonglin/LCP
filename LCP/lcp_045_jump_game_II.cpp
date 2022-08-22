fa7194893703826dcf53f2bef1ec12b367aa8bd7
jiahong <jiahonglin17@gmail.com>
2022-08-23 00:07:38 +0800

    LeedCode Practice question lcp_045_jump_game_II

--- LCP/lcp_045_jump_game_II.cpp
+++ LCP/lcp_045_jump_game_II.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+
+    int jump(vector<int>& nums){
+        int n = nums.size();
+        vector<int> v(n,0);
+        for(int i=n-2;i>=0;i--){
+            if(nums[i] >= n-1-i){
+                v[i] = 1;
+            }
+            else{
+                int minStep = INT_MAX-1;
+                for(int j=1;j<=nums[i];j++)
+                    minStep = min(minStep,1+v[i+j]);
+                v[i] = minStep;
+            }
+        }
+        return v[0];
+    }
+
     int jump(vector<int>& nums) {
         int jumpNum=0, curStep=0, maxStep=0;
         for(int i=0;i<nums.size()-1;i++){
