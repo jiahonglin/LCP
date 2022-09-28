6f95f29c9a5ece6ab0db1411da6b41fdc47f9031
jiahong <jiahonglin17@gmail.com>
2022-09-29 00:57:59 +0800

    LeedCode Practice question lcp_063_Unique_Paths2

--- LCP/lcp_063_Unique_Paths2.cpp
+++ LCP/lcp_063_Unique_Paths2.cpp
@@ -4,6 +4,50 @@ public:
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
+        return cal(obstacleGrid,dp,m-1,n-1);
+    }
+    int cal(vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp, int m, int n){
+        if(dp[m][n] != -1) return dp[m][n];
+        if(obstacleGrid[m][n] == 1) return dp[m][n]=0;
+        if(m==0 && n==0) return dp[m][n]=1;
+    
+        int ans=0;
+        if(m>0) ans+=cal(obstacleGrid,dp,m-1,n);
+        if(n>0) ans+=cal(obstacleGrid,dp,m,n-1);
+        return dp[m][n] = ans;
+    }
+        /*
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j] == 1){
+                    obstacleGrid[i][j]=0;
+                    continue;
+                }
+                if(i-1 >= 0  && j-1 >= 0){
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
+                }
+                else if(i-1>=0){
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j];
+                }
+                else if(j-1>=0){
+                    obstacleGrid[i][j] = obstacleGrid[i][j-1];
+                }
+                else{
+                    obstacleGrid[i][j]=1;
+                }
+            }
+        }
+        return obstacleGrid[m-1][n-1];
+    }
+    */
+/*
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
         return findPaths(obstacleGrid,0,0,dp);
     }
     int findPaths(vector<vector<int>>& Grid, int x, int y, vector<vector<int>>& dp){
@@ -15,4 +59,5 @@ public:
         dp[x][y]=findPaths(Grid,x+1,y,dp)+findPaths(Grid,x,y+1,dp);
         return dp[x][y];
     }
+*/
 };
