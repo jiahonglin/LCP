215ff6b362b8adaf08684f05e77d774e50ed4f57
jiahong <jiahonglin17@gmail.com>
2022-10-05 20:03:24 +0800

    LeedCode Practice question lcp_071_simplify_path

--- LCP/lcp_071_simplify_path.cpp
+++ LCP/lcp_071_simplify_path.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     string simplifyPath(string path) {
+        int idx=0;
+        stack<string> dirs;
+        string result="";
+
+        for(int idx=0; idx < path.length();idx++){
+            if(path[idx] == '/') continue;
+            string directory="";
+            while(idx < path.length() && path[idx]!='/') {
+                directory+=path[idx++];
+            }
+            if(directory == "." || directory.length()==0){}
+            else if (directory ==".."){
+                if(!dirs.empty()){
+                    dirs.pop();
+                }
+            }
+            else{
+                dirs.push(directory);
+            }
+        }
+        if(dirs.empty()) return "/";
+        while(!dirs.empty()){
+            result="/"+dirs.top()+result;
+            dirs.pop();
+        }
+        return result;
+    }
+    
+    /*
+    string simplifyPath(string path) {
         
         string directory, result;
         stack<string> s;
@@ -28,4 +58,5 @@ public:
         }
         return result;
     }
+    */
 };
