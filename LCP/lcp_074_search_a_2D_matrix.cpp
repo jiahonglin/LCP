83ecce81270fbeeeaef11b91b1e897d723da8c75
jiahong <jiahonglin17@gmail.com>
2022-10-11 23:42:41 +0800

    LeedCode Practice question lcp_074_search_a_2D_matrix

--- LCP/lcp_074_search_a_2D_matrix.cpp
+++ LCP/lcp_074_search_a_2D_matrix.cpp
@@ -3,6 +3,46 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
+        bool bFind = false;
+        if(m == 1){
+            for(int j=0;j<n;j++){
+                if(matrix[0][j] == target){
+                    bFind=true;
+                    break;
+                } 
+            }
+            return bFind;
+        }
+
+        if(n == 1){
+            for(int i=0;i<m;i++){
+                if(matrix[i][0] == target){
+                    bFind=true;
+                    break;
+                }
+            }
+            return bFind;
+        }
+
+        for(int i=0;i<m;i++){
+            if(matrix[i][0] <= target && target <= matrix[i][n-1]){
+                for(int j=0;j<n;j++){
+                    if(matrix[i][j] == target){
+                        bFind=true;
+                        break;
+                    }
+                }
+                return bFind;
+            }
+        }
+
+        return bFind;
+
+    }
+    /*
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
         int i=0;
         for(;i<m;i++){
             if(matrix[i][0] <= target && matrix[i][n-1] >= target)
@@ -17,4 +57,5 @@ public:
 
         
     }
+    */
 };
