f29a59f451ccd445a6a44559082f773bf8eaa4de
jiahong <jiahonglin17@gmail.com>
2022-11-28 23:26:37 +0800

    LeedCode Practice question lcp_097_interleaving_string

--- LCP/lcp_097_interleaving_string.cpp
+++ LCP/lcp_097_interleaving_string.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
+        if(s1.length() + s2.length() != s3.length()) return false;
+        int n1 = s1.length();
+        int n2 = s2.length();
+        vector<vector<bool>> dp(n1+1,vector<bool>(n2+1,false));
+        dp[0][0] = true;
+
+        for(int j=1; j<=n2; j++){
+            dp[0][j] = (dp[0][j-1] && s2[j-1] == s3[j-1]);
+        }
+        for(int i=1; i<=n1; i++){
+            dp[i][0] = (dp[i-1][0] && s1[i-1] == s3[i-1]);
+        }
+        for(int i=1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
+                dp[i][j] = ((dp[i][j-1] && s2[j-1] == s3[j-1+i]) || (dp[i-1][j] && s1[i-1] == s3[i-1+j]));
+            }
+        }
+        return dp[n1][n2];
+    }
+
+    /*
+    bool isInterleave(string s1, string s2, string s3) {
         int l1=s1.length();
         int l2=s2.length();
         int l3=s3.length();
@@ -19,4 +41,5 @@ public:
         return false;
         
     }
+    */
 };
