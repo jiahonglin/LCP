5c8dc0ca8b47f0d1b33c54935071c2aba0771033
jiahong <jiahonglin17@gmail.com>
2023-05-24 23:28:44 +0800

    LeedCode Practice question lcp_209_minimum_size_subarray_sum

--- LCP/lcp_209_minimum_size_subarray_sum.cpp
+++ LCP/lcp_209_minimum_size_subarray_sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     int minSubArrayLen(int target, vector<int>& nums) {
         int i=0,j=0;
         int sum=0;
@@ -17,5 +18,21 @@ public:
             j++;
         }
         return ans==INT_MAX?0:ans;
+    }
+*/
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int left=0,right=0;
+        int sum=0;
+        int len=INT_MAX;
+        for(int right=0;right<nums.size();right++){
+            sum+=nums[right];
+            while(left<=right && sum >= target){
+                len = min(len,right-left+1);
+                sum-=nums[left];
+                left++;
+            }
+        }
+        return (len==INT_MAX)?0:len;
+
     }
 };
