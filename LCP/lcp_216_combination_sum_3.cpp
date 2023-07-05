08db832217ddca589688db624fa21d77cc0c31c2
jiahong <jiahonglin17@gmail.com>
2023-07-06 00:06:50 +0800

    LeedCode Practice question lcp_216_combination_sum_3

--- LCP/lcp_216_combination_sum_3.cpp
+++ LCP/lcp_216_combination_sum_3.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
     vector<vector<int>> combinationSum3(int k, int n) {
-        int minsum = (1+k)*k/2;
-        if(n < minsum) return {};
-        vector<int> combine;
-        doCombination(k,n,1,combine);
+        if(k > n) return {};
+        vector<int> sum;
+        vector<vector<int>> ans;
+        generateSum(k,n,1,sum,ans);
         return ans;
     }
-    void doCombination(int k, int n, int start, vector<int>& combine){
-        if(combine.size() > k) return;
-        if(combine.size() == k){
-            int sum=0;
-            for(int m=0;m<combine.size();m++) sum+=combine[m];
-            if(sum==n) ans.push_back(combine);
-            else return;
+    void generateSum(int k, int n, int start, vector<int>& sum, vector<vector<int>>& ans){
+        if(sum.size() == k && n==0){
+            ans.push_back(sum);
+            return;
         }
+        if(n < 0 || sum.size() > k) return;
         for(int i=start;i<=9;i++){
-            combine.push_back(i);
-            doCombination(k,n,i+1,combine);
-            combine.pop_back();
+            sum.push_back(i);
+            generateSum(k,n-i,i+1,sum,ans);
+            sum.pop_back();
         }
     }
+/*
+    vector<vector<int>> combinationSum3(int k, int n) {
+        int minSum = (k+1)*k/2;
+        if(n < minSum) return {};
+        vector<int> combination;
+        vector<vector<int>> ans;
+        docombination(k,n,1,combination,ans);
+        return ans;
+    }
+    void docombination(int k, int n, int start, vector<int>& combination, vector<vector<int>>& ans){
+        if(combination.size() > k) return;
+        if(combination.size() == k){
+            int sum=0;
+            for(int i=0;i<combination.size();i++) sum+=combination[i];
+            if(sum == n) ans.push_back(combination);
+            return; 
+        }
+        for(int j=start;j<=9;j++){
+            combination.push_back(j);
+            docombination(k,n,j+1,combination,ans);
+            combination.pop_back();
+        }
+        
+    }
+    */
 };
