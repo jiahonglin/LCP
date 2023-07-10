4d6ccf18db2f180eac0aaf25e61afb9f628cbcf8
jiahong <jiahonglin17@gmail.com>
2023-07-10 23:49:01 +0800

    LeedCode Practice questionlcp_235_lowest_common_ancestor
    _of_a_binary_search_tree

--- LCP/lcp_235.cpp
+++ LCP/lcp_235.cpp
@@ -7,21 +7,12 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if( (root->val > p->val && root->val < q->val) ||
-            (root->val < p->val && root->val > q->val) ||
-            (root->val == p->val) ||
-            (root->val == q->val))
-        {
-            return root;
-        }
-        else if(root->val > p->val && root->val > q->val)
-            return lowestCommonAncestor(root->left,p,q);
-        else if(root->val < p->val && root->val < q->val)
-            return lowestCommonAncestor(root->right,p,q);
-        else
-            return NULL;
+        if(root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left,p,q);
+        else if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right,p,q);
+        else return root;
     }
 };
