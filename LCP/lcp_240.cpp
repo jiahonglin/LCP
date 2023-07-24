402c85efa69ff51accaf9a60b31729695c9dfba3
jiahong <jiahonglin17@gmail.com>
2023-07-24 23:04:15 +0800

    LeedCode Practice question lcp_240_search_a_2D_matrix_2

--- LCP/lcp_240.cpp
+++ LCP/lcp_240.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.empty()) return false;
-        return searchRec(matrix,target,0,matrix[0].size()-1,0,matrix.size()-1);
-    }
-    bool searchRec(vector<vector<int>>& matrix,int target, int left, int right, int up, int down){
-        if(left>right || up>down) return false;
-        if(matrix[up][left] > target || matrix[down][right] < target) return false;
-        int mid = left+(right-left)/2;
-        int row=up;
-        while(row <= down && matrix[row][mid] <= target){
-            if(matrix[row][mid] == target) return true;
-            row++;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int startR = m-1;
+        int startC = 0;
+        if(matrix.size()==0 || matrix[0].size()==0) return false;
+        if(target < matrix[0][0] || target > matrix[m-1][n-1]) return false;
+
+        while(true){
+            if(target > matrix[startR][startC]) startC++;
+            else if(target < matrix[startR][startC]) startR--;
+            else return true;
+            if(startR < 0 || startC >= n) break;
         }
-     return searchRec(matrix,target,left,mid-1,row,down) || searchRec(matrix,target,mid+1,right,up,row-1);
+        return false;
     }
-    
     /*
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-        if(matrix.size() == 0) return false;
-        int x = matrix.size()-1;
-        int y = 0;
-        while(true){
-            if(matrix[x][y] < target) y++;
-            else if(matrix[x][y] > target) x--;
-            else return true;
-            if(x<0||y>=matrix[0].size()) break;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i = 0;i<m;i++){
+            if(target >= matrix[i][0] && target <= matrix[i][n-1]){
+                for(int j=0;j<n;j++){
+                    if(target == matrix[i][j]) return true;
+                }
+            }
         }
         return false;
     }
