1805972511193dd91103ced834bdd35302481e98
jiahong <jiahonglin17@gmail.com>
2023-08-16 23:34:14 +0800

    LeedCode Practice question lcp_284_peeking_iterator

---
+++ LCP/lcp_284_peeking_iterator.cpp
@@ -0,0 +1,50 @@
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *  public:
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator {
+public:
+    bool flag;
+    int value;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    flag = false;
+      value=0;
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        if(!flag) value = Iterator::next();
+        flag=true;
+        return value;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    if(!flag) value = Iterator::next();
+        flag=false;
+        return value;
+	}
+	
+	bool hasNext() const {
+	    return flag || Iterator::hasNext();
+	}
+};
