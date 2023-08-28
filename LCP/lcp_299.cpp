ceb9becca08f0547f49bd5f85050b67439283cc1
jiahong <jiahonglin17@gmail.com>
2023-08-28 23:51:05 +0800

    LeedCode Practice question lcp_299_bulls_and_cows

--- LCP/lcp_299.cpp
+++ LCP/lcp_299.cpp
@@ -1,53 +1,37 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        unordered_map<char,int> m;
-        unordered_map<char,int> g;
-        int bulls=0;
-        int cows=0;
-        int n=secret.length();
-        int i =0;
-        while(i<n){
-            if(secret[i]==guess[i]){
-                bulls++;
-                i++;
-                continue;
+        int m[256] = {0}, bulls = 0, cows = 0;
+        for (int i = 0; i < secret.size(); ++i) {
+            if (secret[i] == guess[i]) ++bulls;
+            else {
+                if (m[secret[i]]++ < 0) ++cows;
+                if (m[guess[i]]-- > 0) ++ cows;
             }
-            m[secret[i]]++;
-            g[guess[i]]++;
-            i++;
         }
-        for(auto it:m){
-            int x=min(g[it.first],it.second);
-            cows+=x;
-        }
-        return to_string(bulls)+"A"+to_string(cows)+"B";
+        return to_string(bulls) + "A" + to_string(cows) + "B";
     }
-    /*Method 2*/
-    /*
+/*
     string getHint(string secret, string guess) {
-        unordered_map<char,int> m;
-        unordered_map<char,int> g;
+        map<int,int> m;
         int bulls=0;
         int cows=0;
-        for(char c:secret) m[c]++;
-        for(char c:guess) g[c]++;
-
         for(int i=0;i<secret.length();i++){
-            if(secret[i] == guess[i]){
+            if(guess[i]==secret[i]){
                 bulls++;
-                m[guess[i]]--;
-                g[guess[i]]--;
+            }
+            else{
+                m[secret[i]-'0']++;
             }
         }
-        for(int i=0;i<secret.length();i++){
-            if(g[guess[i]]>0 && m[guess[i]] > 0){
-                m[guess[i]]--;
-                g[guess[i]]--;
-                cows++; 
+        for(int i=0;i<guess.length();i++){
+            if(guess[i] != secret[i] && m[guess[i]-'0'] > 0){
+                cows++;
+                m[guess[i]-'0']--;
             }
         }
         return to_string(bulls)+"A"+to_string(cows)+"B";
+
     }
-    */
+*/
 };
