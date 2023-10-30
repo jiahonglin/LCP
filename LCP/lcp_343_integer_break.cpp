50bf6fc3d880735393fed3f8b025df542bd79773
jiahong <jiahonglin17@gmail.com>
2023-10-30 22:04:12 +0800

    LeedCode Practice question lcp_343_integer_break

--- LCP/lcp_343_integer_break.cpp
+++ LCP/lcp_343_integer_break.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int integerBreak(int n) {
+        if(n == 2 || n==3) return n-1;
+        int res = 1;
+        while(n > 4){
+            res*=3;
+            n-=3;
+        }
+        return res*n;
+    }
+/*
+    int integerBreak(int n) {
         vector<int> dp(n+1,1);
         for(int i = 3; i <= n; i++){
             for(int j=1; j < i; j++){
@@ -9,4 +19,5 @@ public:
         }
         return dp[n];
     }
+*/
 };
