24f3cb093f0a0d6e3f9c8c00cb91cfdc78ab3a87
jiahong <jiahonglin17@gmail.com>
2023-11-13 23:28:43 +0800

    LeedCode Practice question lcp_371_sum_of_two_integer

--- LCP/lcp_371_sum_of_two_integer.cpp
+++ LCP/lcp_371_sum_of_two_integer.cpp
@@ -6,17 +6,13 @@ public:
         for(int i=0;i<32;i++){
             int a_bit = a & (1<<i)?1:0;
             int b_bit = b & (1<<i)?1:0;
-            ans |= (a_bit ^ b_bit ^ carry) << i;
-            if(a_bit == 0 && b_bit == 0 && carry == 0||
-              a_bit == 0 && b_bit == 1 && carry == 0 ||
-               a_bit == 1 && b_bit == 0 && carry == 0 ||
-               a_bit == 0 && b_bit == 0 && carry == 1
-              ){
-                carry=0;
-            }
-            else{
-                carry=1;
-            }
+            ans |= ((a_bit ^ b_bit ^ carry) << i);
+            int onecnt=0;
+            if(a_bit == 1) onecnt++;
+            if(b_bit == 1) onecnt++;
+            if(carry == 1) onecnt++;
+            if(onecnt >= 2) carry=1;
+            else carry=0;
         }
         return ans;
     }
